8f8863db661cebb11ad318339b2c9000117f8989
feandrade02 <fe.a.andrade@hotmail.com>
2024-04-23 18:23:53 -0300

    Update usuarios.py

--- back-end/models/usuarios.py
+++ back-end/models/usuarios.py
@@ -1,5 +1,7 @@
 from sqlalchemy import String, Integer, Column, TIMESTAMP, text
 from .database import Base, get_db
+from schemas.users import UsuariosSchema
+from typing import List
 
 class Usuarios(Base):
     __tablename__ = 'usuarios'
@@ -11,7 +13,7 @@ class Usuarios(Base):
     added_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('Now()'))
 
 # Função para inserir um novo usuário
-def inserir_usuario(usuario_data: UsuarioSchema):
+def inserir_usuario(usuario_data: UsuariosSchema):
     session = get_db()
     novo_usuario = Usuarios(nome=usuario_data.nome, email=usuario_data.email, senha=usuario_data.senha)
     session.add(novo_usuario)
@@ -19,14 +21,14 @@ def inserir_usuario(usuario_data: UsuarioSchema):
     session.close()
 
 # Função para obter todos os usuários
-def obter_usuarios() -> List[UsuarioSchema]:
+def obter_usuarios() -> List[UsuariosSchema]:
     session = get_db()
     usuarios = session.query(Usuarios).all()
     session.close()
-    return [UsuarioSchema(nome=usuario.nome, email=usuario.email, senha=usuario.senha) for usuario in usuarios]
+    return [UsuariosSchema(nome=usuario.nome, email=usuario.email, senha=usuario.senha) for usuario in usuarios]
 
 # Função para atualizar um usuário existente
-def atualizar_usuario(usuario_id: int, usuario_data: UsuarioSchema):
+def atualizar_usuario(usuario_id: int, usuario_data: UsuariosSchema):
     session = get_db()
     usuario = session.query(Usuarios).filter_by(id=usuario_id).first()
     if usuario:
