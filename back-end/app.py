aad336fd6840d5db4e559b891be201bc5b7ff63c
RafaelCamposTXR <102463380+RafaelCamposTXR@users.noreply.github.com>
2024-04-23 15:24:42 -0300

    retirando temporariamente a rota de aeroportos

--- back-end/app.py
+++ back-end/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from typing import Optional
-from routers.voos import router as router_lista_aeroportos
+#from routers.voos import router as router_lista_aeroportos
 #from routers.aeroportos import router as router_aeroportos
 
 
