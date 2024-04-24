a63993dc93d09dc8dee5746caa1b29b19f57ea5e
RafaelCamposTXR <102463380+RafaelCamposTXR@users.noreply.github.com>
2024-04-23 22:19:06 -0300

    Update database.py

--- back-end/models1/database.py
+++ back-end/models1/database.py
@@ -3,7 +3,9 @@ import psycopg2
 conn = psycopg2.connect(host="10.18.0.20", dbname="bd_trabalho_asa", user="postgres", password="banco123", port=5432)
 
 cur = conn.cursor
-cur.execute("CREATE TABLE IF NOT EXISTS teste (id INT PRIMARY_KEY)")
+cur.execute("CREATE TABLE IF NOT EXISTS voos (id INT PRIMARY_KEY, aeroporto_saida INT)")
+
+cur.execute("INSERT INTO voos(id, aeroporto_saida) VALUES (0,1))")
 
 conn.commit()
 
